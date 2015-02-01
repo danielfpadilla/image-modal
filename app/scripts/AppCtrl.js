08540d78ac4591a4eefa66f4c120796b8673124d
Daniel F. Padilla <danielfpadilla@gmail.com>
2015-02-01 22:35:13 +0800

    Improve image modal options using schema form

--- app/scripts/AppCtrl.js
+++ app/scripts/AppCtrl.js
@@ -5,16 +5,26 @@ angular.module('AppCtrl',[])
 .controller('AppCtrl',
             ['$scope',
              function ($scope) {
+    $scope.model = {
+        "enableRemove": true,
+        "enableUpload": true,
+        "enableZoom": true
+    };
 
     $scope.form = [
         {
             "key": "profpic",
             "type": "imagemodal",
+            "title": "Profile Pic",
+            "description": "My Face",
             "format": "image",
-            "enableZoom": true,
-            "enableUpload": true,
-            "enableRemove": true
+            "enableUpload": $scope.model.enableUpload,
+            "enableRemove": $scope.model.enableRemove,
+            "enableZoom": $scope.model.enableZoom
         },
+        "enableRemove",
+        "enableUpload",
+        "enableZoom",
         "name",
         "email",
         {
@@ -33,10 +43,17 @@ angular.module('AppCtrl',[])
         "type": "object",
         "title": "Comment",
         "properties": {
-            "profpic": {
-                "title": "Profile Pic",
-                "type": "imagemodal",
-                "description": "My Face"
+            "enableUpload": {
+                "type": "boolean",
+                "title": "Enable Upload"
+            },
+            "enableRemove": {
+                "type": "boolean",
+                "title": "Enable Remove"
+            },
+            "enableZoom": {
+                "type": "boolean",
+                "title": "Enable Zoom"
             },
             "name": {
                 "title": "Name",
@@ -62,5 +79,11 @@ angular.module('AppCtrl',[])
         ]
     };
 
-    $scope.model = {};
+    $scope.$watch('model', function (value) {
+        if (value) {
+            $scope.form[0].enableRemove = value.enableRemove;
+            $scope.form[0].enableUpload = value.enableUpload;
+            $scope.form[0].enableZoom = value.enableZoom;
+        }
+    }, true);
 }])
